48b67a687371d1742ee1ab8330b1621fc5d098ef
Alex Tim <buildmnt@gmail.com>
2022-05-04 14:46:07 +0300

    ver

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import { astroImageTools } from 'astro-imagetools';
 
+
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    integrations: true,
+    integrations: true
   },
-
   integrations: [astroImageTools],
 });
